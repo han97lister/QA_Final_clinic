8c29c6b34da47f781f1f96e069ae2099ba39dd2b
Ubuntu <joshua@new.pdc1b4uxcm2ublkmnpvb0jbhih.zx.internal.cloudapp.net>
2021-01-11 15:59:24 +0000

    terraform

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # QA_Final_clinic
+
+Terraform - Terraform allows you to control your infrastructure on the cloud service provider.
+
+the reason we used terraform is 
---
+++ terraform/main.tf
@@ -0,0 +1,29 @@
+provider "azurerm"{
+	features{}
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "lastproject"
+  location = "uksouth"
+}
+
+resource "azurerm_kubernetes_cluster" "example" {
+  name                = "cluster"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  dns_prefix          = "exampleaks1"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_D2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = {
+    Environment = "Production"
+  }
+}
