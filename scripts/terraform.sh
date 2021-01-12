108e47530aded14739e53e9e45043d8817f5c5f8
Ubuntu <hlsims@project.etswurrwqi0u5ootiyn0nnxooh.zx.internal.cloudapp.net>
2021-01-12 15:32:04 +0000

    testing edit to to script

--- scripts/terraform.sh
+++ scripts/terraform.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
-az aks get-credentials --resource-group lastproject --name cluster
 cd terraform
 terraform init
 terraform plan
 terraform apply -auto-approve
 cd ..
+az aks get-credentials --resource-group lastproject --name cluster
