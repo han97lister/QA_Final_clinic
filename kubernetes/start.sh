c96ee3ece4ae35b2dc0eb2c9cbca5b555c51ff40
Ubuntu <alexazure@VM1.3wx3a3s50zjehkjqddenxpa23g.zx.internal.cloudapp.net>
2021-01-14 17:30:36 +0000

    hotfix

--- kubernetes/start.sh
+++ kubernetes/start.sh
@@ -5,4 +5,4 @@ kubectl apply -f back.yaml
 kubectl apply -f front.yaml
 kubectl apply -f nginx.yaml
 
-kubectl rolling restart deployment mysql.yaml back.yaml front.yaml nginx.yaml
+kubectl rollout restart deployment mysql.yaml back.yaml front.yaml nginx.yaml
